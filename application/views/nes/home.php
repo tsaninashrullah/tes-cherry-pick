69db4bd4d9c1027fa11e42e609fa61d3d3525175
Tsani <tsani.n@smooets.com>
2017-01-04 13:58:42 +0700

    2

--- application/views/nes/home.php
+++ application/views/nes/home.php
@@ -1,6 +1,15 @@
+tentunaskk asdaksd nkaskdakl; sdk as; ldasd kl;asd;l kasda nl;sdas lkd
+tentunaskk asdaksd nkaskdakl; sdk as; ldasd kl;asd;l kasda nl;sdas lkd
+tentunaskk asdaksd nkaskdakl; sdk as; ldasd kl;asd;l kasda nl;sdas lkd
 asdhsouiahsdaisdha sdiaso dpwqpda sdaghsdiu wqhw dqhakj dasdj 
-asdhsouiahsdaisdha sdiaso dpwqpda sdaghsdiu wqhw dqhakj dasdj 
-asdhsouiahsdaisdha sdiaso dpwqpda sdaghsdiu wqhw dqhakj dasdj 
-asdhsouiahsdaisdha sdiaso dpwqpda sdaghsdiu wqhw dqhakj dasdj 
-asdhsouiahsdaisdha sdiaso dpwqpda sdaghsdiu wqhw dqhakj dasdj 
+tentunaskk asdaksd nkaskdakl; sdk as; ldasd kl;asd;l kasda nl;sdas lkd
+tentunaskk asdaksd nkaskdakl; sdk as; ldasd kl;asd;l kasda nl;sdas lkd
+tentunaskk asdaksd nkaskdakl; sdk as; ldasd kl;asd;l kasda nl;sdas lkd
+tentunaskk asdaksd nkaskdakl; sdk as; ldasd kl;asd;l kasda nl;sdas lkd
 <h2>GANTENG</h2>
+tentunaskk asdaksd nkaskdakl; sdk as; ldasd kl;asd;l kasda nl;sdas lkd
+tentunaskk asdaksd nkaskdakl; sdk as; ldasd kl;asd;l kasda nl;sdas lkd
+tentunaskk asdaksd nkaskdakl; sdk as; ldasd kl;asd;l kasda nl;sdas lkd
+tentunaskk asdaksd nkaskdakl; sdk as; ldasd kl;asd;l kasda nl;sdas lkd
+tentunaskk asdaksd nkaskdakl; sdk as; ldasd kl;asd;l kasda nl;sdas lkd
+tentunaskk asdaksd nkaskdakl; sdk as; ldasd kl;asd;l kasda nl;sdas lkd
