fcdd9147d47f76ecafd2ed25147f17ececeb8455
Tsani <tsani.n@smooets.com>
2017-01-04 14:01:44 +0700

    3

--- application/views/nes/home.php
+++ application/views/nes/home.php
@@ -7,9 +7,15 @@ tentunaskk asdaksd nkaskdakl; sdk as; ldasd kl;asd;l kasda nl;sdas lkd
 tentunaskk asdaksd nkaskdakl; sdk as; ldasd kl;asd;l kasda nl;sdas lkd
 tentunaskk asdaksd nkaskdakl; sdk as; ldasd kl;asd;l kasda nl;sdas lkd
 <h2>GANTENG</h2>
-tentunaskk asdaksd nkaskdakl; sdk as; ldasd kl;asd;l kasda nl;sdas lkd
-tentunaskk asdaksd nkaskdakl; sdk as; ldasd kl;asd;l kasda nl;sdas lkd
-tentunaskk asdaksd nkaskdakl; sdk as; ldasd kl;asd;l kasda nl;sdas lkd
+asdasdjiq iqwjdq owjasdlkha sdakjsdhad asdnjainag
+
+askldaskdoasdkoll
+askldaskdoasdkoll
+askldaskdoasdkoll
+askldaskdoasdkoll
+askldaskdoasdkoll
+askldaskdoasdkoll
+askldaskdoasdkoll
 tentunaskk asdaksd nkaskdakl; sdk as; ldasd kl;asd;l kasda nl;sdas lkd
 tentunaskk asdaksd nkaskdakl; sdk as; ldasd kl;asd;l kasda nl;sdas lkd
 tentunaskk asdaksd nkaskdakl; sdk as; ldasd kl;asd;l kasda nl;sdas lkd
