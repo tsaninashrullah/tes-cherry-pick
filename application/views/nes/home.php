8172bca43b1e3f5ede61aa3dccac9fd36969e0cb
Tsani <tsani.n@smooets.com>
2017-01-04 14:03:11 +0700

    4

--- application/views/nes/home.php
+++ application/views/nes/home.php
@@ -1,14 +1,29 @@
 tentunaskk asdaksd nkaskdakl; sdk as; ldasd kl;asd;l kasda nl;sdas lkd
+askldaskdoasdkoll
+askldaskdoasdkoll
+askldaskdoasdkoll
 tentunaskk asdaksd nkaskdakl; sdk as; ldasd kl;asd;l kasda nl;sdas lkd
 tentunaskk asdaksd nkaskdakl; sdk as; ldasd kl;asd;l kasda nl;sdas lkd
+askldaskdoasdkoll
+askldaskdoasdkoll
 asdhsouiahsdaisdha sdiaso dpwqpda sdaghsdiu wqhw dqhakj dasdj 
 tentunaskk asdaksd nkaskdakl; sdk as; ldasd kl;asd;l kasda nl;sdas lkd
 tentunaskk asdaksd nkaskdakl; sdk as; ldasd kl;asd;l kasda nl;sdas lkd
+askldaskdoasdkoll
+askldaskdoasdkoll
+askldaskdoasdkoll
 tentunaskk asdaksd nkaskdakl; sdk as; ldasd kl;asd;l kasda nl;sdas lkd
 tentunaskk asdaksd nkaskdakl; sdk as; ldasd kl;asd;l kasda nl;sdas lkd
 <h2>GANTENG</h2>
 asdasdjiq iqwjdq owjasdlkha sdakjsdhad asdnjainag
 
+askldaskdoasdkoll
+askldaskdoasdkoll
+askldaskdoasdkoll
+askldaskdoasdkoll
+askldaskdoasdkoll
+askldaskdoasdkoll
+askldaskdoasdkoll
 askldaskdoasdkoll
 askldaskdoasdkoll
 askldaskdoasdkoll
